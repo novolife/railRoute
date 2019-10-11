8a58b2a343d44aae9f22e092a2fb4874a2205a84
Yu1Aragaki <mahajana@foxmail.com>
2019-10-11 18:12:28 +0800

    add editable node name (ongoing)

--- drawitem.cpp
+++ drawitem.cpp
@@ -14,15 +14,63 @@ DrawItem::DrawItem(ItemType itemType, QColor itemColor)
         }
     }
 
-    myText = new QGraphicsSimpleTextItem("bbb");
-
-    myText->setPos(nodeChild->pos().x(), nodeChild->pos().y()+nodeChild->boundingRect().height());
+    TextChild = new QGraphicsSimpleTextItem("name");
+    TextChild->setPos(nodeChild->boundingRect().width(), -10);
 
     this->addToGroup(nodeChild);
-    this->addToGroup(myText);
-    this->setHandlesChildEvents(false);
+    this->addToGroup(TextChild);
+    this->setHandlesChildEvents(true);
 
     setFlag(QGraphicsItem::ItemIsMovable, true);
-    //setFlag(QGraphicsItem::ItemIsSelectable, true);
-    //setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+    setFlag(QGraphicsItem::ItemIsSelectable, true);
+    setFlag(QGraphicsItem::ItemSendsGeometryChanges, true);
+
+    m_cTimer = new QTimer();
+
+    connect(m_cTimer, &QTimer::timeout, this, &DrawItem::slotTimerTimeOut);
+}
+
+void DrawItem::mousePressEvent(QMouseEvent *e)
+{
+    m_cTimer->start(500);
+    qDebug() << "aaaaabb";
+    e->accept();
+}
+
+void DrawItem::mouseDoubleClickEvent(QMouseEvent *e)
+{
+    m_cTimer->stop();
+
+    qDebug() << "aaaaa";
+
+    QString nodeName;
+
+    switch (langType)
+    {
+        case 0:
+        {
+            nodeName = QInputDialog::getText(nullptr, "Input the name", "Input the name of the selecetd node:");
+            break;
+        }
+
+        case 1:
+        {
+            nodeName = QInputDialog::getText(nullptr, QString::fromLocal8Bit("输入名称"), QString::fromLocal8Bit("输入所选节点的名称: "));
+            break;
+        }
+
+        case 2:
+        {
+            nodeName = QInputDialog::getText(nullptr, "Input the name", "Input the name of the selecetd node:");
+            break;
+        }
+    }
+
+    this->TextChild->setText(nodeName);
+    e->accept();
+}
+
+void DrawItem::slotTimerTimeOut()
+{
+    m_cTimer->stop();
 }
--- drawitem.h
+++ drawitem.h
@@ -2,13 +2,19 @@
 #define DRAWITEM_H
 
 #include <QtWidgets>
+#include <QObject>
 #include "nodeitem.h"
+#include "common.h"
 
-class DrawItem : public QGraphicsItemGroup
+class DrawItem : public QObject, public QGraphicsItemGroup
 {
-    Q_GADGET
+    Q_OBJECT
 
 public:
+    /**
+     * @brief The ItemType enum
+     * @note  can add more item
+     */
     enum ItemType
     {
         Node
@@ -18,10 +24,21 @@ public:
     DrawItem(ItemType itemType, QColor itemColor);
     ItemType type() {return myItemType;}
 
+private slots:
+    void slotTimerTimeOut();
+
 private:
+    //using QGraphicsItem::mousePressEvent;
+    //using QGraphicsItem::mouseDoubleClickEvent;
+    using QGraphicsItemGroup::type; // avoid virtual func overload warning
     ItemType myItemType;
     NodeItem *nodeChild;
-    QGraphicsSimpleTextItem *myText;
+    QGraphicsSimpleTextItem *TextChild;
+    QTimer *m_cTimer;
+
+protected:
+    void mousePressEvent(QMouseEvent *e);
+    void mouseDoubleClickEvent(QMouseEvent *event);
 };
 
 #endif // DRAWITEM_H
